e966e9519edafec1d69ab36d0364a9e004998aa1
Tushar Soni <tusharsachin1soni@gmail.com>
2023-09-02 17:49:05 +0530

    Created Catalouge component

---
+++ src/routes/Catalouge.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import ProductCatalouge from '../components/ProductCatalouge'
+
+const Catalouge = () => {
+  return (
+    <div style={{background: "#ccc"}}>
+      <Navbar />
+      <ProductCatalouge />
+    </div>
+  )
+}
+
+export default Catalouge
