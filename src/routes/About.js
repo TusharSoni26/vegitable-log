ef21c512c6632a45129878694ca4636663d40e9a
Tushar Soni <tusharsachin1soni@gmail.com>
2023-09-02 17:48:11 +0530

    remove hero component

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,24 +2,17 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from "../components/Navbar"
 import Hero from '../components/Hero'
+import AboutUs from '../components/AboutUs'
 
 const About = () => {
   return (
     <div>
+        
       <Navbar />
-      <Hero 
-        cName="hero"
-        heroImg="https://plus.unsplash.com/premium_photo-1686529591640-876ef71ab2ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjF8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
-        title="About Us"
-        text = ""
-        url = "/"
-        btnClass="show"
-        btnText = "Subscribe to NewsLetter"
-       />
+       <AboutUs />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default About
-
+export default About;
