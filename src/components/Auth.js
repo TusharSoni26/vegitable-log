5d04205ed25cd414abdf5f67077a6ff65b1edd3b
Tushar Soni <tusharsachin1soni@gmail.com>
2023-09-02 17:55:21 +0530

    Integrated Backend API

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './Auth.css';
+import Spinner from './Spinner';
 
-const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
+const initialState = { name: '', email: '', password: '', conformPassword: '' };
 
 const Auth = () => {
   const [form, setForm] = useState(initialState);
   const [isSignUp, setIsSignUp] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [clicked, setClicked] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
 
   var strClass = "form-group";
 
@@ -18,11 +23,69 @@ const Auth = () => {
     setShowPassword(false);
   }
 
-  function handleChange(e){
-    console.log(e.target.value);
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setForm({...form, [name]:value});
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    setClicked(true);
+    const allFormValue = {"name": form.fullName, "email":form.email, "password":form.password, "conformPassword":form.conformPassword}
+
+    if(isSignUp){
+      try{
+        let res = await fetch("/customer/register", {
+          // mode: 'no-cors',
+          method:"POST",
+          headers:{'content-type':'application/json'},
+          body:JSON.stringify(allFormValue)
+        });
+
+        if(res.status == 200){
+          setLoading(false);
+          localStorage.setItem("isLoggedIn", true);
+          localStorage.setItem("userEmail", allFormValue.email);
+          navigate('/');
+        }
+        else{
+          localStorage.setItem("isLoggedIn", false);
+        }
+      } catch(error) {
+        console.log(error.message);
+      }
+    }
+    else{
+      const loginFormValue = {"email":form.email, "password":form.password}
+      // console.log(loginFormValue);
+      try{
+        let res = await fetch("/customer/login", {
+          // mode: 'no-cors',
+          method:"POST",
+          headers:{'content-type':'application/json'},
+          body:JSON.stringify(loginFormValue)
+        });
+
+        if(res.status == 200){
+          setLoading(false);
+          localStorage.setItem("isLoggedIn", true);
+          localStorage.setItem("userEmail", allFormValue.email);
+          navigate('/');
+        }
+        else{
+          localStorage.setItem("isLoggedIn", false);
+        }
+      } catch(error) {
+        console.log(error.message);
+      }
+    }
   }
 
   const handleShowPassword = () => setShowPassword(!showPassword);
+
+  if(loading && clicked){
+    return (<Spinner speed={1} customText={"Loading..."}/>);
+  }
 
   return (
     <div className='wrapper'>
@@ -31,26 +94,22 @@ const Auth = () => {
         <div className='lock-icon'>
         <i className="fa-solid fa-lock fa-2xl"></i>
         </div>
-        <form className='form-ele'>
+        <form className='form-ele' method='POST' onSubmit={handleSubmit}>
           {isSignUp && (
             <div className='first-row'>
               <div className="form-group">
-                <input name="firstName" className='form-input' onchange={handleChange} autoFocus/>
-                <label>First Name*</label>
-              </div>
-              <div className='form-group'>
-                <input name="lastName" className='form-input'  onchange={handleChange} />
-                <label>Last Name*</label>
+                <input name="fullName" className='form-input' onChange={handleChange} autoFocus/>
+                <label>Full Name*</label>
               </div>
             </div>
           )
           }
           <div className='form-group'>
-            <input name="email" className='form-input' type="email" onchange={handleChange} />
+            <input name="email" className='form-input' type="email" onChange={handleChange} />
             <label>Email*</label>
           </div>
           <div className='form-group spe'>
-            <input name="password" className='form-input' type={showPassword ? 'text' : 'password'} onchange={handleChange}/>
+            <input name="password" className='form-input' type={showPassword ? 'text' : 'password'} onChange={handleChange}/>
             <label>Password*</label>
             <div className='icon-eye' onClick={handleShowPassword}>
               <i className={showPassword ? "fa-solid fa-eye-slash" : "fa-solid fa-eye"} ></i>
@@ -59,7 +118,7 @@ const Auth = () => {
           {
             isSignUp && (
               <div className='form-group'>
-                <input name='confirmPassword' className='form-input' type="password" onchange={handleChange}/>
+                <input name='conformPassword' className='form-input' type="password" onChange={handleChange}/>
                 <label>Confirm Password*</label>
               </div>
             )
@@ -68,8 +127,11 @@ const Auth = () => {
           <button type='submit' className='form-btn signin'>
             {isSignUp?"SignUp":"SignIn"}
           </button>
-
-          <button onClick={switchMode} className='form-btn  acc-change'>
+          {isSignUp?"":
+          <button className='form-btn  acc-change'>
+            Forgot Password?
+          </button>}
+          <button onClick={switchMode} className='form-btn  fg-pass'>
             {isSignUp ? 'Already have an account? Sign In' : "Dont't have an account? Sign Up"}
           </button>
         </form >
