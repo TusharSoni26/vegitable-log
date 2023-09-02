621608ced7cfcd78f629ffab274e44b33bd48936
Tushar Soni <tusharsachin1soni@gmail.com>
2023-09-02 17:50:27 +0530

    Modified Home component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
         heroImg="https://images.unsplash.com/photo-1518843875459-f738682238a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dmVnZXRhYmxlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
         title="Your Health Our Priority"
         text = "Start Purchasing Vegetable Below"
-        url = "/"
+        url = "/catalouge"
         btnClass="show"
         btnText = "Purchase"
        />
