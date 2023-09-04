3076b7adf471ff3c25262e6e94053a7012377be6
Tushar Soni <tusharsachin1soni@gmail.com>
2023-09-04 21:13:16 +0530

    fixed component not loading error

--- src/components/ProductCatalouge.js
+++ src/components/ProductCatalouge.js
@@ -11,6 +11,8 @@ const ProductCatalouge = () => {
   const [loading, setLoading] = useState(true);
   const [minPrice, setMinPrice] = useState(0);
   const [maxPrice, setMaxPrice] = useState(500);
+  const [filters, setFilters] = useState(false);
+  const [filterData, setFilterData] = useState([]);
   const [form, setForm] = useState({
     name: "",
     category: "",
@@ -45,9 +47,10 @@ const ProductCatalouge = () => {
   const applyFilters = async(e) => {
     e.preventDefault();
     setLoading(true);
+    setFilters(true);
 
     const formData = new FormData(e.target);
-    const filterData = {
+    const filterrrData = {
       name: formData.get("name"),
       category: formData.get("category"),
       minPrice: minPrice,
@@ -58,11 +61,11 @@ const ProductCatalouge = () => {
     await fetch("seller/products/filter", {
       method:"POST",
           headers:{'content-type':'application/json'},
-          body:JSON.stringify(filterData)
+          body:JSON.stringify(filterrrData)
     })
       .then((response) => response.json())
       .then((data) => {
-        setProductData(data);
+        setFilterData(data);
         setLoading(false);
         // console.log(data);
       })
@@ -70,11 +73,69 @@ const ProductCatalouge = () => {
   }
 
   const resetFilters = () => {
-    getProductData();
+    setFilters(false);
+    setFilterData([]);
   }
 
   if (loading) {
     return (<Spinner speed={5} customText={"Loading..."} />)
+  }
+
+  if(filters){
+    return(
+      <div className="main-container">
+      <div className="sidebar">
+        <form className="filter-form" onSubmit={applyFilters}>
+          <h3>Filters</h3>
+          <span className="filter-form-span">
+            <label for="name" className="filter-label">
+              Name:
+            </label>
+            <input type="text" name="name" onChange={handleChange}/>
+          </span>
+          <span className="filter-form-span">
+            <label for="category" className="filter-label">
+              Category:
+            </label>
+            <input type="text" name="category" onChange={handleChange}/>
+          </span>
+          <span className="filter-form-span">
+            <label for="minPrice">
+              Min Price
+            </label>
+            <span className="filter-slider">
+              <input
+                type="range" min="0" max="500" name="minPrice" value={minPrice}
+                onChange={(e) => setMinPrice(e.target.value)}
+              />
+              <p id="rangeValue">{minPrice}</p>
+            </span>
+          </span>
+          <span className="filter-form-span">
+            <label for="maxPrice">
+              Max Price
+            </label>
+            <span className="filter-slider">
+              <input
+                type="range" min="0" max="500" name="maxPrice" value={maxPrice}
+                onChange={(e) => setMaxPrice(e.target.value)}
+              />
+              <p id="rangeValue">{maxPrice}</p>
+            </span>
+          </span>
+          <button className="apply-button">Apply Filters</button>
+        </form>
+          <button className="reset-button" onClick={resetFilters}>Reset Filters</button>
+      </div>
+      <div className="product-cat-container">
+        {filterData.map((item, index) => {
+          return (
+            <Product key={index} title={item.name} image={item.image} desc={item.description} price={item.price} email={item.email} />
+          )
+        })}
+      </div>
+    </div>
+    )
   }
 
   return (
